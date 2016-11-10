58898221a3a6603fad519f4eebbd719f3e415b12
Jonathan Doig <z8290362@ad.unsw.edu.au>
2016-11-10 15:15:10 +1100

    Ansible cleanup
    * Moved email address into a variable
    * Combined last three small playbooks
    * Added reminder to configure example.com site

--- install_geonode.sh
+++ install_geonode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo ansible-playbook install_geonode.yml --extra-vars \"host=cftest admin_pass=INSERT_PASSWORD_HERE\"
+echo ansible-playbook install_geonode.yml --extra-vars \"host=cftest.intersect.org.au admin_email=j.doig@unsw.edu.au admin_pass=INSERT_PASSWORD_HERE\"
