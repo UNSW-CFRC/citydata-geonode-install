20ea5b63fea3834651dd7353d174675896643a9e
Jonathan Doig <z8290362@ad.unsw.edu.au>
2016-11-18 10:09:48 +1100

    Made help file generic

--- help.sh
+++ help.sh
@@ -1,1 +1,1 @@
-echo ansible-playbook install_geonode.yml --extra-vars \"host=cftest.intersect.org.au admin_email=j.doig@unsw.edu.au admin_pass=INSERT_PASSWORD_HERE\"
+echo ansible-playbook install_geonode.yml --extra-vars \"host=HOSTNAME admin_email=EMAIL admin_pass=PASSWORD\"
