f0fb33f835e2895fb8a958117c3ec2da11f90f98
Jonathan Doig <z8290362@ad.unsw.edu.au>
2017-05-26 17:20:21 +1000

    Make geonode.yml work in Ansible Best Practice layout

--- help.sh
+++ help.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "ansible-playbook geonode.yml -i [dev | test | prod] --extra-vars \"admin_pass=PASSWORD\" --step --start-at-task=\"FIRST_TASK\""
+echo "ansible-playbook geonode.yml -i [dev | test | prod] --extra-vars=\"admin_pass=PASSWORD\" --step --start-at-task=\"FIRST_TASK\""
