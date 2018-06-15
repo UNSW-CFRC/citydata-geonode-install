4e3e89a80fe5d5bf5e1a7d572da36b9e854ace35
Jonathan Doig <jonathan.doig@gmail.com>
2018-06-15 11:46:20 +1000

    Help: play install_geonode with skip-tags py3

--- help.sh
+++ help.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "ansible-playbook geonode.yml -i [dev | test | prod] --extra-vars=\"admin_pass=PASSWORD secret_key=SECRET_KEY\" --step --start-at-task=\"FIRST_TASK\""
+
+echo "ansible-playbook geonode.yml -i [dev | test | prod] --extra-vars=\"admin_pass=PASSWORD secret_key=SECRET_KEY\" --skip-tags "py3" --step --start-at-task=\"FIRST_TASK\""
