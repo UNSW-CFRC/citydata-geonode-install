781cb9370c64a4a18d20a8dfae711b9a7f8f6603
Jonathan Doig <jonathan.doig@gmail.com>
2018-11-29 16:42:03 +1100

    Delete spare folder
    Note these files are present in the master branch, before today's changes

--- spare/help.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "ansible-playbook geonode.yml -i [dev | test | prod] --extra-vars=\"admin_pass=PASSWORD secret_key=SECRET_KEY\" --skip-tags "py3" --step --start-at-task=\"FIRST_TASK\""
